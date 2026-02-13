50026e1b2f2dd056472d01d6455f02380498827e
GeraldGallet <gerald.gallet.gg@gmail.com>
2026-02-13 13:30:18 +0100

    feat: add web-dev-react-lab folder

---
+++ web-dev-react-lab/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+import { Hello } from "./Hello";
+
+function App() {
+  const [name, setName] = useState<string>("Gérard");
+  const [newName, setNewName] = useState<string>("");
+
+  const onValidate = () => {
+    setName(newName);
+    setNewName("");
+  };
+
+  return (
+    <>
+      <Hello name={name}>
+        <span>Welcome here</span>
+      </Hello>
+      <input
+        value={newName}
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+      />
+      <button onClick={onValidate}>Ok</button>
+    </>
+  );
+}
+
+export default App;
